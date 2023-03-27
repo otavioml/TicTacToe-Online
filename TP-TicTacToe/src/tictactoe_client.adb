f965308c64411ed4de1ccb8740fa7adc4e04bc73
Otávio de Meira Lima <33879489+otavioml@users.noreply.github.com>
2023-03-27 16:31:14 +0200

    Updates on client
    Now the client can send and receive moves and update the board.

--- TP-TicTacToe/src/tictactoe_client.adb
+++ TP-TicTacToe/src/tictactoe_client.adb
@@ -5,24 +5,17 @@ with P_Chaine;              use P_Chaine;
 with Conseil_TicTacToe;     use Conseil_TicTacToe;
 
 procedure TicTacToe_Client is
-    Socket : Socket_Type;
-    Addr   : Sock_Addr_Type;
-    msg    : T_Chaine (500);
-    move   : String := "x y";
-    pos_x  : Integer;
-    pos_y  : Integer;
-    player : Character;
-    Last   : Integer;
-    conseil : T_Conseil := ((' ', ' ', ' '),
+    Socket          : Socket_Type;
+    Addr            : Sock_Addr_Type;
+    move_send       : T_Chaine (500);
+    player          : Character;
+    other_player    : Character;
+    Last            : Integer;
+    conseil         : T_Conseil := ((' ', ' ', ' '),
     (' ', ' ', ' '),
     (' ', ' ', ' '));
+    end_game        : Boolean := False;
 begin
-    --  Put_Piece (conseil, 1, 1, 'X');
-    --  Log_Conseil (conseil);
-    --  delay (1.0);
-    --  Clear_Screen;
-    --  Put_Piece (conseil, 2, 1, 'O');
-    --  Log_Conseil (conseil);
     Create_Socket (Socket, Family_Inet, Socket_Stream);
     Addr.Addr := Addresses (Get_Host_By_Name ("127.0.0.1"), 1);
     Addr.Port := Port_Type'Value ("7777");
@@ -36,27 +29,69 @@ begin
         Put ("You will be player ");
         Put_Line (initial_message);
         player := To_String (initial_message) (1);
+        if player = 'X' then
+            other_player := 'O';
+        else
+            other_player := 'X';
+        end if;
+        Put (other_player);
+
     end;
-    loop
-        -- try two procedures: send and receive
-        -- player 1 will play first. send -> receive
-        -- player 2 will play after. receive -> send
-        Log_Conseil (conseil);
-        Put ("Type the position you want to play : ");
-        Get_Line (msg);
-        --  Update board
-        move := To_String (msg);
-        pos_x := Char_To_Integer (move (1));
-        pos_y := Char_To_Integer (move (3));
-        Clear_Screen;
-        Put_Piece (conseil, pos_x, pos_y, player);
-        --  Envoi
-        Send (Socket, To_String (msg), Last);
-        Put ("Waiting for the other player...");
-        declare
-            msg_rec : constant T_Chaine (500) := Init_Chaine (Recv (Socket));
-        begin
-            Put_Line (msg_rec);
-        end;
-    end loop;
+    if player = 'X' then
+        while end_game = False loop
+            --  try two procedures: send and receive
+            --  player 1 will play first. send -> receive
+            --  player 2 will play after. receive -> send
+            Clear_Screen;
+            Log_Conseil (conseil);
+            Put ("Type the position you want to play : ");
+            Get_Line (move_send);
+            --  Update board
+            Clear_Screen;
+            Put_Piece (conseil, move_send, player);
+            Log_Conseil (conseil);
+            --  Envoi
+            Send (Socket, To_String (move_send), Last);
+            Put ("Waiting for the other player...");
+            declare
+                move_received : constant T_Chaine (500) :=
+                    Init_Chaine (Recv (Socket));
+            begin
+                Clear_Screen;
+                Put_Piece (conseil, move_received, other_player);
+                Log_Conseil (conseil);
+
+            end;
+            end_game := Verify_Win (conseil);
+        end loop;
+    else
+        while end_game = False loop
+                --  try two procedures: send and receive
+                --  player 1 will play first. send -> receive
+                --  player 2 will play after. receive -> send
+                Clear_Screen;
+                Log_Conseil (conseil);
+                Put ("Waiting for the other player...");
+                declare
+                    move_received : constant T_Chaine (500) :=
+                        Init_Chaine (Recv (Socket));
+                begin
+                    Clear_Screen;
+                    Put_Piece (conseil, move_received, other_player);
+                    Log_Conseil (conseil);
+                end;
+                Clear_Screen;
+                Log_Conseil (conseil);
+                Put ("Type the position you want to play : ");
+                Get_Line (move_send);
+                --  Update board
+                Clear_Screen;
+                Put_Piece (conseil, move_send, player);
+                Log_Conseil (conseil);
+                --  Envoi
+                Send (Socket, To_String (move_send), Last);
+                end_game := Verify_Win (conseil);
+        end loop;
+    end if;
+    Put ("Game ended. ");
 end TicTacToe_Client;
