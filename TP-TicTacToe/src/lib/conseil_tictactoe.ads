27182954d4e7556cf24fca0484f8ba1be143b41f
Otávio de Meira Lima <33879489+otavioml@users.noreply.github.com>
2023-03-27 16:33:19 +0200

    New code to verify win

--- TP-TicTacToe/src/lib/conseil_tictactoe.ads
+++ TP-TicTacToe/src/lib/conseil_tictactoe.ads
@@ -1,9 +1,12 @@
+with P_Chaine;      use P_Chaine;
+
 package Conseil_TicTacToe is
 
     type T_Conseil is array (1 .. 3, 1 .. 3) of Character;
 
     procedure Put_Piece (conseil : in out T_Conseil;
-    x : Integer; y : Integer; piece : Character);
+    move : T_Chaine; piece : Character);
     procedure Log_Conseil (conseil : T_Conseil);
+    function Verify_Win (conseil : T_Conseil) return Boolean;
 
 end Conseil_TicTacToe;
