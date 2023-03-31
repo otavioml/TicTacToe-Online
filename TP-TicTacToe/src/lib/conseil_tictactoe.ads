ca676068d612c974f1ec9ec3be3aebc133cc3387
Otávio de Meira Lima <33879489+otavioml@users.noreply.github.com>
2023-03-31 16:29:13 +0200

    Modification of function declaration

--- TP-TicTacToe/src/lib/conseil_tictactoe.ads
+++ TP-TicTacToe/src/lib/conseil_tictactoe.ads
@@ -4,8 +4,8 @@ package Conseil_TicTacToe is
 
     type T_Conseil is array (1 .. 3, 1 .. 3) of Character;
 
-    procedure Put_Piece (conseil : in out T_Conseil;
-    move : T_Chaine; piece : Character);
+    function Put_Piece (conseil : in out T_Conseil;
+    move : T_Chaine; piece : Character) return Boolean;
     procedure Log_Conseil (conseil : T_Conseil);
     function Verify_Win (conseil : T_Conseil) return Boolean;
 
